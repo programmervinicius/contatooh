1bba673d4b74127f9f88e5d110e5ec46fe832973
Dayane Elen <Dayane Elen>
2016-01-21 22:34:00 -0200

    alteração de arquivos

--- app/controllers/contato.js
+++ app/controllers/contato.js
@@ -1,145 +1,155 @@
 //app/controllers/contato.js
 var contatos = [{
-	_id : 1,
-	nome : 'Contato Exemplo 1',
-	email : 'cont1@empresa.com.br'
+    _id : 1,
+    nome : 'Contato Exemplo 1',
+    email : 'cont1@empresa.com.br'
 }, {
-	_id : 2,
-	nome : 'Contato Exemplo 2',
-	email : 'cont2@empresa.com.br'
+    _id : 2,
+    nome : 'Contato Exemplo 2',
+    email : 'cont2@empresa.com.br'
 }, {
-	_id : 3,
-	nome : 'Contato Exemplo 3',
-	email : 'cont3@empresa.com.br'
+    _id : 3,
+    nome : 'Contato Exemplo 3',
+    email : 'cont3@empresa.com.br'
 }, {
-	_id : 4,
-	nome : 'Contato Exemplo 4',
-	email : 'cont4@empresa.com.br'
+    _id : 4,
+    nome : 'Contato Exemplo 4',
+    email : 'cont4@empresa.com.br'
 }, {
-	_id : 1,
-	nome : 'Contato Exemplo 1',
-	email : 'cont1@empresa.com.br'
+    _id : 1,
+    nome : 'Contato Exemplo 1',
+    email : 'cont1@empresa.com.br'
 }, {
-	_id : 2,
-	nome : 'Contato Exemplo 2',
-	email : 'cont2@empresa.com.br'
+    _id : 2,
+    nome : 'Contato Exemplo 2',
+    email : 'cont2@empresa.com.br'
 }, {
-	_id : 3,
-	nome : 'Contato Exemplo 3',
-	email : 'cont3@empresa.com.br'
+    _id : 3,
+    nome : 'Contato Exemplo 3',
+    email : 'cont3@empresa.com.br'
 }, {
-	_id : 4,
-	nome : 'Contato Exemplo 4',
-	email : 'cont4@empresa.com.br'
+    _id : 4,
+    nome : 'Contato Exemplo 4',
+    email : 'cont4@empresa.com.br'
 }, {
-	_id : 1,
-	nome : 'Contato Exemplo 1',
-	email : 'cont1@empresa.com.br'
+    _id : 1,
+    nome : 'Contato Exemplo 1',
+    email : 'cont1@empresa.com.br'
 }, {
-	_id : 2,
-	nome : 'Contato Exemplo 2',
-	email : 'cont2@empresa.com.br'
+    _id : 2,
+    nome : 'Contato Exemplo 2',
+    email : 'cont2@empresa.com.br'
 }, {
-	_id : 3,
-	nome : 'Contato Exemplo 3',
-	email : 'cont3@empresa.com.br'
+    _id : 3,
+    nome : 'Contato Exemplo 3',
+    email : 'cont3@empresa.com.br'
 }, {
-	_id : 4,
-	nome : 'Contato Exemplo 4',
-	email : 'cont4@empresa.com.br'
+    _id : 4,
+    nome : 'Contato Exemplo 4',
+    email : 'cont4@empresa.com.br'
 }, {
-	_id : 1,
-	nome : 'Contato Exemplo 1',
-	email : 'cont1@empresa.com.br'
+    _id : 1,
+    nome : 'Contato Exemplo 1',
+    email : 'cont1@empresa.com.br'
 }, {
-	_id : 2,
-	nome : 'Contato Exemplo 2',
-	email : 'cont2@empresa.com.br'
+    _id : 2,
+    nome : 'Contato Exemplo 2',
+    email : 'cont2@empresa.com.br'
 }, {
-	_id : 3,
-	nome : 'Contato Exemplo 3',
-	email : 'cont3@empresa.com.br'
+    _id : 3,
+    nome : 'Contato Exemplo 3',
+    email : 'cont3@empresa.com.br'
 }, {
-	_id : 4,
-	nome : 'Contato Exemplo 4',
-	email : 'cont4@empresa.com.br'
+    _id : 4,
+    nome : 'Contato Exemplo 4',
+    email : 'cont4@empresa.com.br'
 }, {
-	_id : 1,
-	nome : 'Contato Exemplo 1',
-	email : 'cont1@empresa.com.br'
+    _id : 1,
+    nome : 'Contato Exemplo 1',
+    email : 'cont1@empresa.com.br'
 }, {
-	_id : 2,
-	nome : 'Contato Exemplo 2',
-	email : 'cont2@empresa.com.br'
+    _id : 2,
+    nome : 'Contato Exemplo 2',
+    email : 'cont2@empresa.com.br'
 }, {
-	_id : 3,
-	nome : 'Contato Exemplo 3',
-	email : 'cont3@empresa.com.br'
+    _id : 3,
+    nome : 'Contato Exemplo 3',
+    email : 'cont3@empresa.com.br'
 }, {
-	_id : 4,
-	nome : 'Contato Exemplo 4',
-	email : 'cont4@empresa.com.br'
+    _id : 4,
+    nome : 'Contato Exemplo 4',
+    email : 'cont4@empresa.com.br'
 }, {
-	_id : 1,
-	nome : 'Contato Exemplo 1',
-	email : 'cont1@empresa.com.br'
+    _id : 1,
+    nome : 'Contato Exemplo 1',
+    email : 'cont1@empresa.com.br'
 }, {
-	_id : 2,
-	nome : 'Contato Exemplo 2',
-	email : 'cont2@empresa.com.br'
+    _id : 2,
+    nome : 'Contato Exemplo 2',
+    email : 'cont2@empresa.com.br'
 }, {
-	_id : 3,
-	nome : 'Contato Exemplo 3',
-	email : 'cont3@empresa.com.br'
+    _id : 3,
+    nome : 'Contato Exemplo 3',
+    email : 'cont3@empresa.com.br'
 }, {
-	_id : 4,
-	nome : 'Contato Exemplo 4',
-	email : 'cont4@empresa.com.br'
+    _id : 4,
+    nome : 'Contato Exemplo 4',
+    email : 'cont4@empresa.com.br'
 }, {
-	_id : 1,
-	nome : 'Contato Exemplo 1',
-	email : 'cont1@empresa.com.br'
+    _id : 1,
+    nome : 'Contato Exemplo 1',
+    email : 'cont1@empresa.com.br'
 }, {
-	_id : 2,
-	nome : 'Contato Exemplo 2',
-	email : 'cont2@empresa.com.br'
+    _id : 2,
+    nome : 'Contato Exemplo 2',
+    email : 'cont2@empresa.com.br'
 }, {
-	_id : 3,
-	nome : 'Contato Exemplo 3',
-	email : 'cont3@empresa.com.br'
+    _id : 3,
+    nome : 'Contato Exemplo 3',
+    email : 'cont3@empresa.com.br'
 }, {
-	_id : 4,
-	nome : 'Contato Exemplo 4',
-	email : 'cont4@empresa.com.br'
+    _id : 4,
+    nome : 'Contato Exemplo 4',
+    email : 'cont4@empresa.com.br'
 }, {
-	_id : 1,
-	nome : 'Contato Exemplo 1',
-	email : 'cont1@empresa.com.br'
+    _id : 1,
+    nome : 'Contato Exemplo 1',
+    email : 'cont1@empresa.com.br'
 }, {
-	_id : 2,
-	nome : 'Contato Exemplo 2',
-	email : 'cont2@empresa.com.br'
+    _id : 2,
+    nome : 'Contato Exemplo 2',
+    email : 'cont2@empresa.com.br'
 }, {
-	_id : 3,
-	nome : 'Contato Exemplo 3',
-	email : 'cont3@empresa.com.br'
+    _id : 3,
+    nome : 'Contato Exemplo 3',
+    email : 'cont3@empresa.com.br'
 }, {
-	_id : 4,
-	nome : 'Contato Exemplo 4',
-	email : 'cont4@empresa.com.br'
+    _id : 4,
+    nome : 'Contato Exemplo 4',
+    email : 'cont4@empresa.com.br'
 }];
 
 module.exports = function() {
-	var controller = {};
-	controller.listaContatos = function(req, res) {
-		res.json(contatos);
-	};
-	controller.obtemContato = function(req, res) {
-		var idContato = req.params.id,
-		    contato = contatos.filter(function(contato){
-		return contato._id == idContato;
-		})[0];
-		contato ? res.json(contato) : res.status(404).send('Contato não encontrado.');
-	};
-	return controller;
-}
+    var controller = {};
+
+    controller.listaTodos = function(req, res) {
+        res.json(contatos);
+    };
+
+    controller.obtemContato = function(req, res) {
+        var idContato = req.params.id,
+            contato = contatos.filter(function(contato){
+        return contato._id == idContato;
+        })[0];
+        contato ? res.json(contato) : res.status(404).send('Contato não encontrado.');
+    };
+
+    controller.removeContato = function(req, res) {
+        var idContato = req.params.id;
+        contatos = contatos.filter(function(contato) {
+            return contato._id != idContato;
+        });
+        res.send(204).end();
+    };
+    return controller;
+};
--- app/routes/contato.js
+++ app/routes/contato.js
@@ -1,6 +1,10 @@
 //app/routes/contato.js
 module.exports = function(app) {
-	var controller = app.controllers.contato;
-	app.get('/contatos', controller.listaContatos);
-	app.get('/contatos/:id', controller.obtemContato);
-}; 
+    var controller = app.controllers.contato;
+    /*app.route('/contatos');
+    app.route('/contatos/:id');*/
+    app.get('/contatos', controller.listaTodos);
+    //app.post('/contatos', controller.salvaContato);
+    app.get('/contatos/:id', controller.obtemContato);
+    app.delete('/contatos/:id', controller.removeContato);
+};
